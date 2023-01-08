8d21bf71d06c8138393ef8aa7c1634403a438a2a
Hyeonbin Kim <bin3635@gmail.com>
2023-01-08 21:16:41 +0900

    Add Vuetify 3

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,19 @@ import router from './router'
 
 import './assets/main.css'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const app = createApp(App)
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.use(vuetify).mount('#app')
